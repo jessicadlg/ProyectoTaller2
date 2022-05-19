0ac6c75a54a7a3be8c63e4a394b958f8c0dc2e5c
Agustina <agustinabbellone@gmail.com>
2022-05-19 15:10:04 -0300

    conexión con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDf8OEJXvSIhxniEjjFaiP6v3NJNkIBtwk",
+    authDomain: "tallerweb2-1f7fb.firebaseapp.com",
+    projectId: "tallerweb2-1f7fb",
+    storageBucket: "tallerweb2-1f7fb.appspot.com",
+    messagingSenderId: "202341473104",
+    appId: "1:202341473104:web:1d726d80c1fea211baaa55"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDf8OEJXvSIhxniEjjFaiP6v3NJNkIBtwk",
+    authDomain: "tallerweb2-1f7fb.firebaseapp.com",
+    projectId: "tallerweb2-1f7fb",
+    storageBucket: "tallerweb2-1f7fb.appspot.com",
+    messagingSenderId: "202341473104",
+    appId: "1:202341473104:web:1d726d80c1fea211baaa55"
+  }
 };
 
 /*
