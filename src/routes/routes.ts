44415ea3c871102c8a03d948a79521373d2e48d2
jessicadlg <jessica_dlg@live.com.ar>
2022-05-24 15:31:18 -0300

    estructura inicial,bootstrap y fontawesone

---
+++ src/routes/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { SigninComponent } from "src/app/views/signin/signin.component";
+
+export const appRoutes: Routes = [
+    { path: 'signin', component: SigninComponent },
+];
